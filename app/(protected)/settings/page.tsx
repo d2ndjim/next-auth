067d5b5cb4ba4a6f1bd326bc82fcbe41a76da218
Lekan <jimohridwanlekan@gmail.com>
2024-01-26 22:54:05 +0100

    Add token callback

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,16 @@
-const Settings = () => {
+import { signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+
+const Settings = async () => {
   return (
     <div>
       <h1>Settings</h1>
+      <form action={async () => {
+        "use server"
+        await signOut()
+      }}>
+        <Button>Sign Out</Button>
+      </form>
     </div>
   );
 };
