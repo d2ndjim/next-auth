c0930da6bb193ec99c8d3d37030f70e358ef8923
Lekan <jimohridwanlekan@gmail.com>
2024-01-28 14:28:39 +0100

    Add email verification for register

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,7 @@ import { db } from '@/lib/db';
 
 import { RegisterSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
+import { generateVerificationToken } from '@/lib/tokens';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -30,5 +31,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     },
   });
 
-  return { success: 'User created' };
+  const verificationToken = await generateVerificationToken(email);
+
+  return { success: 'Confirmation email sent!' };
 };
